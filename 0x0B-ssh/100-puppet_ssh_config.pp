bb7ec2c49deaa14d711bca1a10a31e64107c7a9b
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-26 09:31:58 +0000

    Puppet ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,8 +2,10 @@
 exec { 'sed':
   command  => "sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' ~/.ssh/config",
   provider => 'shell',
+  onlyif   => "test -e ~/.ssh/config",
 }
-exec { 'sed':
+exec { 'sed1':
   command  => "sudo sed -i 's#IdentityFile ~/.ssh/id_rsa#IdentityFile ~/.ssh/school#' ~/.ssh/config",
   provider => 'shell',
+  onlyif   => "test -e ~/.ssh/config",
 }
