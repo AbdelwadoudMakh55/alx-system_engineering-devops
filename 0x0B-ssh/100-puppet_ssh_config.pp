6618c9cd827ef0b165be84924a084dca29af4337
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-26 10:03:00 +0000

    Puppet ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # Puppet ssh config
 exec { 'disable_password_authentication':
-  command  => "echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config",
+  command  => "echo 'PasswordAuthentication no' >> ~/.ssh/config",
   provider => 'shell',
 }
 exec { 'set_identity_file':
-  command  => "echo 'IdentityFile ~/.ssh/school' >> /etc/ssh/sshd_config",
+  command  => "echo 'IdentityFile ~/.ssh/school' >> ~/.ssh/config",
   provider => 'shell',
 }
