916a97975bc1bcc13b12d7efd4d619e5c4b94ba4
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-08 23:08:07 +0000

    HTTP header fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # Automating the installation and the set up of the nginx server
 exec { 'create IP adress variable':
-  command  => 'IP_Adress=$(hostname -I | awk "{print $1}")',
+  command  => 'IP_Address=$(hostname -I | awk "{print $1}")',
   provider => 'shell',
 }
 exec { 'create host variable':
@@ -24,17 +24,17 @@ exec { 'Hello world':
   provider => 'shell',
 }
 exec { 'redirection':
-  command  => "echo 'server {
+  command  => 'echo "server {
     listen 80 ;
     listen [::]:80;
     root /var/www/html;
     index index.html;
-    server_name ${host};
-    add_header X-Served-By ${hostname};
+    server_name $host;
+    add_header X-Served-By $hostname;
     location /redirect_me {
         return 301 /;
     }
-}' | sudo tee /etc/nginx/sites-available/default",
+}" | sudo tee /etc/nginx/sites-available/default',
   provider => 'shell',
 }
 exec { 'Restart nginx':
