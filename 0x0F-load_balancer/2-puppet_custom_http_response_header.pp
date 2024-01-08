2a2483a47b7380a8b8c4d21f564ec9c93d3bdf89
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-08 22:12:29 +0000

    Puppet automation

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,35 @@
+# Automating the installation and the set up of the nginx server
+exec { 'update apt-get':
+  command  => 'sudo apt-get update',
+  provider => 'shell',
+}
+exec { 'install nginx':
+  command  => 'sudo apt-get install -y nginx',
+  provider => 'shell',
+}
+exec { 'start nginx':
+  command  => 'sudo service nginx start',
+  provider => 'shell',
+}
+exec { 'Hello world':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => 'shell',
+}
+exec { 'redirection':
+  command  => 'echo "server {
+    listen 80 ;
+    listen [::]:80;
+    root /var/www/html;
+    index index.html;
+    server_name $(hostname -I);
+    add_header X-Served-By $hostname;
+    location /redirect_me {
+        return 301 /;
+    }
+}" | sudo tee /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+exec { 'Restart nginx':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
+}
