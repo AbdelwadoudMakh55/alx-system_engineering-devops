ac0ee98c48ef3686768500ffc5a3fda54361c54a
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-29 11:45:17 +0000

    Fixing username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     url_info = 'https://jsonplaceholder.typicode.com/users?id='
     url_todos = 'https://jsonplaceholder.typicode.com/todos?userId='
     r_user = requests.get(url_info + sys.argv[1])
-    username = r_user.json()[0].get("name")
+    username = r_user.json()[0].get("username")
     r_todos = requests.get(url_todos + sys.argv[1])
     with open(sys.argv[1] + ".csv", "a") as f:
         for task in r_todos.json():
