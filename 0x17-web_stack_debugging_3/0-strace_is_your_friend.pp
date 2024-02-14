15b13dc53735c682aead29b89d063216358db8b5
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-02-14 12:43:36 +0000

    Fixing the WordPress website

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,9 @@
 # Fixing the .php extension
 exec { '.phpp':
-  command  => "sed 's/phpp/php/g' /var/www/html/wp-settings.php",
+  command  => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  provider => 'shell',
+}
+exec { 'restart':
+  commmand => 'sudo service apache2 restart',
   provider => 'shell',
 }
