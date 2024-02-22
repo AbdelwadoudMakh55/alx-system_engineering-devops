ed3a3de07a3448d8bbb9e24e15c49e0c4508145a
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-02-22 10:20:25 +0000

    Fixing ULIMIT

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Fixing ULIMIT for nginx
 exec { 'ULIMIT':
-  command  => 'sed -i s/ULIMIT=\"-n 15\"//g /etc/default/nginx',
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"//g" /etc/default/nginx',
   provider => 'shell',
 }
 exec { 'Restart_nginx':
