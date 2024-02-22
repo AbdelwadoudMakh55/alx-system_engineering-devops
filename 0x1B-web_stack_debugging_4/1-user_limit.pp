c60901406f28911333f3e1edcbbac1b97c34275d
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-02-22 11:26:43 +0000

    open files limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Fixing limit for holberton user
+exec { 'hard_limit':
+  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 1000/g" /etc/security/limits.conf',
+  provider => 'shell',
+}
+exec { 'soft_limit':
+  command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 1000/g" /etc/security/limits.conf',
+  provider => 'shell',
+}
+exec { 'reload':
+  command  => 'bash --login',
+  provider => 'shell',
+}
